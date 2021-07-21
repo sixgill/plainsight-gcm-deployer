ec9fbf006d349550aa2aeca431931d900ad8b0ac
Jordan Gregory <jgregory@sixgill.com>
2021-07-21 15:00:21 -0500

    things

---
+++ scripts/upload_images.sh
@@ -0,0 +1,17 @@
+while IFS= read -r line || [[ -n "$line" ]]
+do
+    echo "$line"
+    svc=$(echo $line | awk -F '[/:]' '{ print $4 }')
+    if [ $svc = "latest" ]
+    then
+        svc=$(echo $line | awk -F '[/:]' '{ print $3 }') 
+    fi
+    if [ $svc = "sense-billing-service" ]
+    then
+        svc="billing"
+    fi
+    newsvc="gcr.io/plainsight-public/images/$svc:1.0"
+    docker pull $line
+    docker tag $line $newsvc
+    docker push $newsvc
+done < "$1"
