c926112d0985ba46227725fe87cee8c657dc65d9
Jordan Gregory <jgregory@sixgill.com>
2021-07-26 15:17:16 -0500

    add additional service

--- scripts/fetch_vendor_folders.sh
+++ scripts/fetch_vendor_folders.sh
@@ -4,5 +4,10 @@ DEPLOYERPATH=$GO_DEVPATH/plainsight-marketplace-deployer
 rm -rf docs
 
 mkdir -p $DEPLOYERPATH/docs
+
+cd $GO_DEVPATH/sense-api && git stash && git checkout main && git pull && go mod vendor
+cd $GO_DEVPATH/sense-billing-service && git stash && git checkout master && git pull && go mod vendor
+cd $DEPLOYERPATH
+
 cp -r $GO_DEVPATH/sense-api/vendor/* $DEPLOYERPATH/docs/
 cp -r $GO_DEVPATH/sense-billing-service/vendor/* $DEPLOYERPATH/docs/
